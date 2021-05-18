7980f7fc80df48f7ef6e52b3607d8c6c169810d6
Tuan Nguyen <tuannva92@gmail.com>
2021-05-18 19:56:37 +0700

    cài đặt và cấu hình kết nối với firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,32 @@ module.exports = {
   siteMetadata: {
     title: "sample",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: "gatsby-firesource",
+      options: {
+        credential: require("./fb-tk.json"),
+		types: [
+			{
+				type: 'Book',
+				collection: 'books',
+				map: doc => ({
+					title: doc.title,
+					summary: doc.summary,
+					author___NODE: doc.author.id
+				})
+			},
+			{
+				type: 'Author',
+				collection: 'authors',
+				map: doc => ({
+					name: doc.name,
+					
+				})
+			}
+		]
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>Nguyen Van Anh Tuan.</p>
+      <p>Nguyễn Văn Anh Tuấn.</p>
     </Layout>
   )
 }
